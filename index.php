4ed6ef01912d670f3c4eee75ba2f520ef2fa3080
brunof19d <brunof19d@gmail.com>
2020-08-05 23:48:03 +0100

    Add grid layout

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Bruno Dadario</title>
+</head>
+
+<header>
+</header>
+
+<body>
+    <div class="main-container">
+
+        <div class="container-left">LEFT</div>
+
+        <div class="container-right">
+            
+            <div class="container-right-top">RIGHT top</div>
+
+            <div class="container-right-down">RIGHT down</div>
+
+        </div>
+
+
+    </div>
+</body>
+
+<footer>teste</footer>
+
+</html>
