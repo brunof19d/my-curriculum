ebf59d69ef8c122cd2b211ca56387341ea28c16b
brunof19d <brunof19d@gmail.com>
2020-08-06 02:17:37 +0100

    Update template

--- index.php
+++ index.php
@@ -5,29 +5,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Bruno Dadario</title>
+    <title>Your Title</title>
 </head>
 
-<header>
-</header>
-
 <body>
-    <div class="main-container">
+<header></header>
 
-        <div class="container-left">LEFT</div>
+<div class="main-container">
 
-        <div class="container-right">
-            
-            <div class="container-right-top">RIGHT top</div>
+    <div class="container-left">
 
-            <div class="container-right-down">RIGHT down</div>
-
+        <div class="img-menu">
+            <h2>My Name</h2>
         </div>
 
+        <div class="info-menu" >
+            <p>Web Developer</p>
+            <p>Porto, Portugal</p>
+            <p>test@test.com</p>
+            <p>315 999 999 999</p>
+            <hr>
+
+            <p class="p-large">Skills</p>
+            <p class="p-medium">Front End</p>
+            <p>HTML, CSS3, JavaScript and Bootstrap</p>
+            <p>Framework: jQuery</p>
+            <p class="p-medium">Back End</p>
+            <p>PHP, C and C++</p>
+            <p>Framework: Laravel</p>
+            <p class="p-medium">Database and SQL</p>
+            <p>MySQL, PostgreSQL and SQL Server</p>
+            <p> NoSQL: MongoDB</p>
+            <hr>
+
+            <p class="p-large">Languages</p>
+            <p>Portuguese</p>
+            <p>English</p>
+        </div>
 
     </div>
-</body>
 
-<footer>teste</footer>
+    <div class="container-right">
+        <div class="container-right-top">RIGHT top</div>
+        <div class="container-right-down">RIGHT down</div>
+    </div>
+
+</div>
+
+<footer>By me</footer>
+</body>
 
 </html>
