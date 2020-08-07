d5e1d6ac8f6278becd0eca95174d2db5d35193fc
brunof19d <brunof19d@gmail.com>
2020-08-07 02:14:12 +0100

    Update template

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Your Title</title>
 </head>
 
@@ -20,10 +21,12 @@
         </div>
 
         <div class="info-menu" >
-            <p>Web Developer</p>
-            <p>Porto, Portugal</p>
-            <p>test@test.com</p>
-            <p>315 999 999 999</p>
+            <p><img src="img/briefcase.png" class="img-icon">
+            Web Developer</p>
+            <p><img src="img/home.png" class="img-icon">
+            Porto, Portugal</p>
+            <p><i class="fa fa-envelope-o"></i>test@test.com</p>
+            <p><img src="img/smartphone-6.png" class="img-icon">315 999 999 999</p>
             <hr>
 
             <p class="p-large">Skills</p>
@@ -34,8 +37,7 @@
             <p>PHP, C and C++</p>
             <p>Framework: Laravel</p>
             <p class="p-medium">Database and SQL</p>
-            <p>MySQL, PostgreSQL and SQL Server</p>
-            <p> NoSQL: MongoDB</p>
+            <p>MySQL, PostgreSQL, SQL Server and MongoDB</p>
             <hr>
 
             <p class="p-large">Languages</p>
@@ -46,13 +48,58 @@
     </div>
 
     <div class="container-right">
-        <div class="container-right-top">RIGHT top</div>
-        <div class="container-right-down">RIGHT down</div>
+
+        <div class="container-right-top">
+            <p class="p-large"><img src="img/briefcase.png" class="img-icon-high">
+            Work Experience<p>
+
+            <p><b>Web Developer / Freelance</b></p>
+            <p><img src="img/calendar.png" class="img-icon"><b>Jun 2020 - Current</b></p>
+
+            <p class="text-explain">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius varius lorem vel lacinia. 
+            Donec egestas mauris tortor, nec consequat lorem rhoncus id. 
+            Maecenas consequat varius lorem, ac pretium ante.</p>
+            <hr>
+
+            <p><b>Civil CAD Designer and Product Development / Tuboar.com.br</b></p>
+            <p><img src="img/calendar.png" class="img-icon"><b>April 2014 - May 2020</b></p>
+
+            <p class="text-explain">Quisque maximus sem lorem, a elementum libero auctor quis. 
+            Mauris erat turpis, tristique ut fermentum in, viverra eu enim. 
+            Donec venenatis tortor non ex cursus, quis sollicitudin mauris tincidunt. 
+            Suspendisse hendrerit, est id sodales malesuada, nunc magna sagittis mi, nec volutpat nunc libero at mi.</p>
+        </div>
+
+        <div class="container-right-down">
+            <p class="p-large"><img src="img/diploma.png" class="img-icon-high">Education<p>
+
+            <p><b>Universidade Paulista - UNIP</b></p>
+            <p><img src="img/calendar.png" class="img-icon"><b>2012 - 2017</b></p>
+
+            <p class="text-explain">Bachelor of Science Degree in Civil Engineering</p>
+            <hr>
+
+            <p><b>Mater Amabilis</b></p>
+            <p><img src="img/calendar.png" class="img-icon"><b>2009 - 2011</b></p>
+
+            <p class="text-explain">Computer technical course.</p>
+
+            <p class="p-large"><img src="img/reading.png" class="img-icon-high">Courses<p>
+
+            <p><b>Impacta</b></p>
+            <p><b>Alura</b></p>
+            <p><b>Caelum</b></p>
+            <p><b>Udemy</b></p>
+        </div>
+      
     </div>
 
 </div>
 
-<footer>By me</footer>
+<footer>
+<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+</footer>
+
 </body>
 
 </html>
