d44b5063d7701696c44a48f16cd33d1a95221850
brunof19d <brunof19d@gmail.com>
2020-08-15 23:30:39 +0100

    Update template

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Your Title</title>
+    <title>My Curriculum</title>
 </head>
 
 <body>
@@ -16,17 +15,18 @@
 
     <div class="container-left">
 
+        <!-- Photo and Name -->
         <div class="img-menu">
             <h2>My Name</h2>
         </div>
 
+        <!-- Info Label -->
         <div class="info-menu" >
-            <p><img src="img/briefcase.png" class="img-icon">
-            Web Developer</p>
-            <p><img src="img/home.png" class="img-icon">
-            Porto, Portugal</p>
-            <p><i class="fa fa-envelope-o"></i>test@test.com</p>
-            <p><img src="img/smartphone-6.png" class="img-icon">315 999 999 999</p>
+            <p>
+                <i class="fas fa-briefcase"></i> Web Developer</p>
+            <p><i class="fas fa-home"></i> Porto, Portugal</p>
+            <p><i class="far fa-envelope"></i> test@test.com</p>
+            <p><i class="fas fa-phone"></i> 315 999 999 999</p>
             <hr>
 
             <p class="p-large">Skills</p>
@@ -49,12 +49,16 @@
 
     <div class="container-right">
 
+        <!-- Work Label -->
         <div class="container-right-top">
-            <p class="p-large"><img src="img/briefcase.png" class="img-icon-high">
-            Work Experience<p>
+
+            <p class="icon-large">
+                <i class="fas fa-briefcase" class="img-icon-high"></i>
+                <b class="text-high">Work Experience</b>
+            <p>
 
             <p><b>Web Developer / Freelance</b></p>
-            <p><img src="img/calendar.png" class="img-icon"><b>Jun 2020 - Current</b></p>
+            <p><img src="img/calendar.png" class="img-icon"><b>Jun 2020 - <span class="box-work">Current</span></b></p>
 
             <p class="text-explain">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius varius lorem vel lacinia. 
             Donec egestas mauris tortor, nec consequat lorem rhoncus id. 
@@ -70,36 +74,43 @@
             Suspendisse hendrerit, est id sodales malesuada, nunc magna sagittis mi, nec volutpat nunc libero at mi.</p>
         </div>
 
+        <!-- Education Label -->
         <div class="container-right-down">
-            <p class="p-large"><img src="img/diploma.png" class="img-icon-high">Education<p>
+
+            <p class="icon-large">
+                <i class="fas fa-university"></i>
+                <b class="text-high">Education</b>
+            <p>
 
             <p><b>Universidade Paulista - UNIP</b></p>
-            <p><img src="img/calendar.png" class="img-icon"><b>2012 - 2017</b></p>
+            <p><i class="far fa-calendar-alt"></i><b> 2012 - 2017</b></p>
 
             <p class="text-explain">Bachelor of Science Degree in Civil Engineering</p>
             <hr>
 
             <p><b>Mater Amabilis</b></p>
-            <p><img src="img/calendar.png" class="img-icon"><b>2009 - 2011</b></p>
+            <p><i class="far fa-calendar-alt"></i><b> 2009 - 2011</b></p>
 
-            <p class="text-explain">Computer technical course.</p>
+            <p class="text-explain">Computer technical course.</p><hr>
 
-            <p class="p-large"><img src="img/reading.png" class="img-icon-high">Courses<p>
-
-            <p><b>Impacta</b></p>
-            <p><b>Alura</b></p>
-            <p><b>Caelum</b></p>
-            <p><b>Udemy</b></p>
+            <p class="icon-large">
+                <i class="fas fa-graduation-cap"></i>
+                <b class="text-high">Courses</b>
+            <p>
+                
+            <a href="#" class="myButton">Alura</a>
+            <a href="#" class="myButton">Caelum</a>
+            <a href="#" class="myButton">Coursera</a>
+            <a href="#" class="myButton">Impacta</a>
+            <a href="#" class="myButton">Udemy</a>
         </div>
-      
     </div>
-
 </div>
 
 <footer>
-<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    AHSUAHSUAHSUAHSAUHSUASHAUSHAUSHAUSHAUSH
 </footer>
 
 </body>
-
+<script src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
 </html>
