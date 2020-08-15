c0e91c3f0350011ff171f85818ac72edbd8beddc
brunof19d <brunof19d@gmail.com>
2020-08-15 23:58:39 +0100

    update template

--- index.php
+++ index.php
@@ -22,25 +22,30 @@
 
         <!-- Info Label -->
         <div class="info-menu" >
-            <p>
-                <i class="fas fa-briefcase"></i> Web Developer</p>
-            <p><i class="fas fa-home"></i> Porto, Portugal</p>
-            <p><i class="far fa-envelope"></i> test@test.com</p>
-            <p><i class="fas fa-phone"></i> 315 999 999 999</p>
+            <p><i class="fas fa-briefcase icon-medium"></i> Web Developer</p>
+            <p><i class="fas fa-home icon-medium"></i> Porto, Portugal</p>
+            <p><i class="far fa-envelope icon-medium"></i> test@test.com</p>
+            <p><i class="fas fa-phone icon-medium"></i> 315 999 999 999</p>
             <hr>
 
-            <p class="p-large">Skills</p>
-            <p class="p-medium">Front End</p>
+            <p>
+                <i class="fas fa-bullseye icon-medium"></i> 
+                <b class="text-medium">Skills</b>
+            </p>
+            <p class="p-medium"><b>Front End</b></p>
             <p>HTML, CSS3, JavaScript and Bootstrap</p>
-            <p>Framework: jQuery</p>
-            <p class="p-medium">Back End</p>
+            <p>Framework: jQuery</p><hr>
+            <p class="p-medium"><b>Back End</b></p>
             <p>PHP, C and C++</p>
-            <p>Framework: Laravel</p>
-            <p class="p-medium">Database and SQL</p>
+            <p>Framework: Laravel</p><hr>
+            <p class="p-medium"><b>Database and SQL</b></p>
             <p>MySQL, PostgreSQL, SQL Server and MongoDB</p>
             <hr>
 
-            <p class="p-large">Languages</p>
+            <p>
+                <i class="fas fa-globe-europe icon-medium"></i>
+                <b class="text-medium">Languages</b>
+            </p>
             <p>Portuguese</p>
             <p>English</p>
         </div>
@@ -48,7 +53,6 @@
     </div>
 
     <div class="container-right">
-
         <!-- Work Label -->
         <div class="container-right-top">
 
@@ -57,23 +61,28 @@
                 <b class="text-high">Work Experience</b>
             <p>
 
-            <p><b>Web Developer / Freelance</b></p>
-            <p><img src="img/calendar.png" class="img-icon"><b>Jun 2020 - <span class="box-work">Current</span></b></p>
+            <p class="p-medium"><b>Web Developer / Freelance</b></p>
+            <p>
+                <i class="far fa-calendar-alt icon-medium"></i>
+                <b>Jun 2020 - <span class="box-work">Current</span></b>
+            </p>
 
             <p class="text-explain">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius varius lorem vel lacinia. 
             Donec egestas mauris tortor, nec consequat lorem rhoncus id. 
             Maecenas consequat varius lorem, ac pretium ante.</p>
             <hr>
 
-            <p><b>Civil CAD Designer and Product Development / Tuboar.com.br</b></p>
-            <p><img src="img/calendar.png" class="img-icon"><b>April 2014 - May 2020</b></p>
+            <p class="p-medium"><b>Civil CAD Designer and Product Development / Tuboar.com.br</b></p>
+            <p>
+                <i class="far fa-calendar-alt icon-medium"></i>
+                <b> April 2014 - May 2020</b>
+            </p>
 
             <p class="text-explain">Quisque maximus sem lorem, a elementum libero auctor quis. 
             Mauris erat turpis, tristique ut fermentum in, viverra eu enim. 
             Donec venenatis tortor non ex cursus, quis sollicitudin mauris tincidunt. 
             Suspendisse hendrerit, est id sodales malesuada, nunc magna sagittis mi, nec volutpat nunc libero at mi.</p>
         </div>
-
         <!-- Education Label -->
         <div class="container-right-down">
 
@@ -82,14 +91,14 @@
                 <b class="text-high">Education</b>
             <p>
 
-            <p><b>Universidade Paulista - UNIP</b></p>
-            <p><i class="far fa-calendar-alt"></i><b> 2012 - 2017</b></p>
+            <p class="p-medium"><b>Universidade Paulista - UNIP</b></p>
+            <p><i class="far fa-calendar-alt icon-medium"></i><b> 2012 - 2017</b></p>
 
             <p class="text-explain">Bachelor of Science Degree in Civil Engineering</p>
             <hr>
 
-            <p><b>Mater Amabilis</b></p>
-            <p><i class="far fa-calendar-alt"></i><b> 2009 - 2011</b></p>
+            <p class="p-medium"><b>Mater Amabilis</b></p>
+            <p><i class="far fa-calendar-alt icon-medium"></i><b> 2009 - 2011</b></p>
 
             <p class="text-explain">Computer technical course.</p><hr>
 
@@ -108,7 +117,6 @@
 </div>
 
 <footer>
-    AHSUAHSUAHSUAHSAUHSUASHAUSHAUSHAUSHAUSH
 </footer>
 
 </body>
