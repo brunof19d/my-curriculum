2eb16a6025b46e9262cc80e10139d2684078e3f0
brunof19d <brunof19d@gmail.com>
2020-09-30 03:21:27 +0100

    separate front end files

---
+++ resources/view/templates/admin/section/basic-data.php
@@ -0,0 +1,55 @@
+<div class="edit-basic">
+
+    <h4 class="text-center">Basic Data</h4>
+
+    <!-- Photo -->
+    <div class="form-group">
+        <label>Photo Profile</label>
+        <input name="photo_profile" type="file" class="form-control-file"">
+    </div>
+
+    <!--  Label Name  -->
+    <div class="form-group">
+        <label>Name:</label>
+        <input type="text" class="form-control">
+    </div>
+
+    <div class="form-row">
+
+        <!--  Profession -->
+        <div class="form-group col-md-6">
+            <label>Profession:</label>
+            <input type="text" class="form-control w-75">
+            <small id="emailHelp" class="form-text text-muted">Example: Web Developer.</small>
+        </div>
+
+        <!--  City and Country -->
+        <div class="form-group col-md-3">
+            <label>City:</label>
+            <input type="text" class="form-control w-75">
+        </div>
+        <div class="form-group col-md-3">
+            <label>Country:</label>
+            <input type="text" class="form-control w-75">
+        </div>
+
+    </div>
+
+    <div class="form-row">
+        <!--  Email -->
+        <div class="form-group col-md-6">
+            <label>Email:</label>
+            <input type="text" class="form-control w-75">
+        </div>
+
+        <!--  Phone -->
+        <div class="form-group col-md-6">
+            <label>Phone:</label>
+            <input type="text" class="form-control w-75">
+            <small id="emailHelp" class="form-text text-muted">Example: 315 999 999 999</small>
+        </div>
+    </div>
+
+    <hr>
+
+</div>
---
+++ resources/view/templates/admin/section/skills.php
@@ -0,0 +1,47 @@
+<div class="edit-skills">
+
+    <h4 class="text-center">Skills</h4>
+
+    <div class="d-flex justify-content-center justify-content-lg-around mt-3">
+        <!--  FrontEnd -->
+        <div class="card m-1">
+            <p class="m-2 font-weight-bold text-info">FrontEnd</p>
+            <div class="d-block m-2">
+                <input type="checkbox" value="" id="defaultCheck1">
+                <label for="defaultCheck12">HTML5</label>
+            </div>
+            <div class="d-block m-2">
+                <input type="checkbox" value="" id="defaultCheck1">
+                <label for="defaultCheck12">CSS</label>
+            </div>
+        </div>
+
+        <!--  BackEnd -->
+        <div class="card m-1">
+            <p class="m-2 font-weight-bold text-info">BackEnd</p>
+            <div class="d-block m-2">
+                <input name="backend_skill_php" type="checkbox" value="1" id="defaultCheck1">
+                <label for="defaultCheck13">PHP</label>
+            </div>
+            <div class="d-block m-2">
+                <input name="backend_skill_c++" type="checkbox" value="2" id="defaultCheck1">
+                <label for="defaultCheck13">C++</label>
+            </div>
+        </div>
+
+
+        <!--  Database -->
+        <div class="card m-1">
+            <p class="m-2 font-weight-bold text-info">Database</p>
+            <div class="d-block m-2">
+                <input type="checkbox" value="" id="defaultCheck1">
+                <label for="defaultCheck1">MySQL</label>
+            </div>
+            <div class="d-block m-2">
+                <input type="checkbox" value="" id="defaultCheck1">
+                <label for="defaultCheck1">PostgreSQL</label>
+            </div>
+        </div>
+    </div>
+    <hr>
+</div>
